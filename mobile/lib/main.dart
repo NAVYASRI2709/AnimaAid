de9049e066de2d1850b03204f662b55f1b6dc0a2
Navya Sri <navyasribotcha2709@gmail.com>
2026-07-18 20:55:18 +0530

    feat: initialize dependency injection

--- mobile/lib/main.dart
+++ mobile/lib/main.dart
@@ -1,6 +1,12 @@
 import 'package:flutter/material.dart';
-import 'app/app.dart';
 
-void main() {
+import 'app/app.dart';
+import 'core/di/injection_container.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await configureDependencies();
+
   runApp(const AnimaAidApp());
 }
